10af01a10567ebd49eee1c8fea340ea5de221527
Behruz Erkinov <100506519+chieftain0@users.noreply.github.com>
2025-11-03 12:28:50 +0400

    bug and type fixes

--- Core/Src/games.c
+++ Core/Src/games.c
@@ -25,6 +25,7 @@
 
 // Snake defines
 #define SNAKE_SPEED 200
+#define HEAD 0
 
 // Pong defines
 #define PONG_SPEED 100
@@ -36,7 +37,59 @@
 #define JUMP 3
 
 // Tetris defines
+uint8_t tetromino[7][4][4] = {
+    // I
+    {
+        {1, 0, 0, 0},
+        {1, 0, 0, 0},
+        {1, 0, 0, 0},
+        {1, 0, 0, 0},
+    },
+    // O
+    {
+        {1, 1, 0, 0},
+        {1, 1, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+    },
+    // T
+    {
+        {1, 1, 1, 0},
+        {0, 1, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+    },
+    // S
+    {
+        {0, 1, 1, 0},
+        {1, 1, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+    },
+    // Z
+    {
+        {1, 1, 0, 0},
+        {0, 1, 1, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+    },
+    // L
+    {
+        {1, 0, 0, 0},
+        {1, 0, 0, 0},
+        {1, 0, 0, 0},
+        {1, 1, 0, 0},
+    },
+    // J
+    {
+        {0, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 0, 0},
+        {1, 1, 0, 0},
+    },
+};
 
+// Numbers
 #define N0 {{1, 1, 1}, {1, 0, 1}, {1, 0, 1}, {1, 0, 1}, {1, 1, 1}}
 #define N1 {{0, 0, 1}, {0, 0, 1}, {0, 0, 1}, {0, 0, 1}, {0, 0, 1}}
 #define N2 {{1, 1, 1}, {0, 0, 1}, {1, 1, 1}, {1, 0, 0}, {1, 1, 1}}
@@ -120,9 +173,9 @@ uint8_t smile_matrix[SCREEN_HEIGHT][SCREEN_WIDTH] = {
 
 void clear_screen(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH])
 {
-    for (int i = 0; i < SCREEN_HEIGHT; i++)
+    for (uint32_t i = 0; i < SCREEN_HEIGHT; i++)
     {
-        for (int j = 0; j < SCREEN_WIDTH; j++)
+        for (uint32_t j = 0; j < SCREEN_WIDTH; j++)
         {
             matrix[i][j] = 0;
         }
@@ -141,9 +194,9 @@ void clear_screen(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH])
  */
 void HeartMatrix(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH])
 {
-    for (int i = 0; i < SCREEN_HEIGHT; i++)
+    for (uint32_t i = 0; i < SCREEN_HEIGHT; i++)
     {
-        for (int j = 0; j < SCREEN_WIDTH; j++)
+        for (uint32_t j = 0; j < SCREEN_WIDTH; j++)
         {
             matrix[i][j] = heart_matrix[i][j];
         }
@@ -162,9 +215,9 @@ void HeartMatrix(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH])
  */
 void SmileMatrix(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH])
 {
-    for (int i = 0; i < SCREEN_HEIGHT; i++)
+    for (uint32_t i = 0; i < SCREEN_HEIGHT; i++)
     {
-        for (int j = 0; j < SCREEN_WIDTH; j++)
+        for (uint32_t j = 0; j < SCREEN_WIDTH; j++)
         {
             matrix[i][j] = smile_matrix[i][j];
         }
@@ -183,7 +236,7 @@ void SmileMatrix(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH])
  *
  * @return -1 if the game is still running, otherwise the score
  */
-int Play_Snake(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint8_t button_mask_click, uint32_t time_now)
+int32_t Play_Snake(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint8_t button_mask_click, uint32_t time_now)
 {
     static uint32_t prev_time = 0;
 
@@ -202,16 +255,16 @@ int Play_Snake(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint8_t button_mask_
         clear_screen(matrix);
 
         // Set all snake LYX to -1
-        for (int i = 0; i < SCREEN_HEIGHT * SCREEN_WIDTH; i++)
+        for (uint32_t i = 0; i < SCREEN_HEIGHT * SCREEN_WIDTH; i++)
         {
             snakeLYX[i][Y] = -1;
             snakeLYX[i][X] = -1;
         }
         // Set the first 2 LYX in the middle facing down
-        snakeLYX[0][0] = 6;
-        snakeLYX[0][1] = 7;
-        snakeLYX[1][0] = 5;
-        snakeLYX[1][1] = 7;
+        snakeLYX[HEAD][Y] = 6;
+        snakeLYX[HEAD][X] = 7;
+        snakeLYX[HEAD + 1][Y] = 5;
+        snakeLYX[HEAD + 1][X] = 7;
 
         first_time_running = 0;
     }
@@ -221,9 +274,9 @@ int Play_Snake(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint8_t button_mask_
     {
         uint8_t foodY_temp = rand() % SCREEN_HEIGHT;
         uint8_t foodX_temp = rand() % SCREEN_WIDTH;
-        for (int i = 0; i < length; i++)
+        for (uint32_t i = 0; i < length; i++)
         {
-            if (snakeLYX[i][0] == foodY_temp && snakeLYX[i][1] == foodX_temp)
+            if (snakeLYX[i][Y] == foodY_temp && snakeLYX[i][X] == foodX_temp)
             {
                 return -1;
             }
@@ -276,47 +329,47 @@ int Play_Snake(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint8_t button_mask_
         clear_screen(matrix);
 
         // Move the snake's head
-        prev_segmentYX[Y] = snakeLYX[Y][0];
-        prev_segmentYX[X] = snakeLYX[Y][1];
-        snakeLYX[Y][0] += directionYX[Y];
-        snakeLYX[Y][1] += directionYX[X];
+        prev_segmentYX[Y] = snakeLYX[HEAD][Y];
+        prev_segmentYX[X] = snakeLYX[HEAD][X];
+        snakeLYX[HEAD][Y] += directionYX[Y];
+        snakeLYX[HEAD][X] += directionYX[X];
 
         // Wrap the snake's head
-        snakeLYX[Y][0] = (snakeLYX[Y][0] + SCREEN_HEIGHT) % SCREEN_HEIGHT;
-        snakeLYX[Y][1] = (snakeLYX[Y][1] + SCREEN_WIDTH) % SCREEN_WIDTH;
+        snakeLYX[HEAD][Y] = (snakeLYX[HEAD][Y] + SCREEN_HEIGHT) % SCREEN_HEIGHT;
+        snakeLYX[HEAD][X] = (snakeLYX[HEAD][X] + SCREEN_WIDTH) % SCREEN_WIDTH;
 
         // Check if snake eats itself
-        for (int i = 1; i < length; i++)
+        for (uint32_t i = 1; i < length; i++)
         {
-            if (snakeLYX[Y][0] == snakeLYX[i][0] && snakeLYX[Y][1] == snakeLYX[i][1])
+            if (snakeLYX[HEAD][Y] == snakeLYX[i][Y] && snakeLYX[HEAD][X] == snakeLYX[i][X])
             {
                 return (length - 2);
             }
         }
 
         // Check if food is eaten
-        if (snakeLYX[Y][0] == foodYX[Y] && snakeLYX[Y][1] == foodYX[X])
+        if (snakeLYX[HEAD][Y] == foodYX[Y] && snakeLYX[HEAD][X] == foodYX[X])
         {
             length++;
             spawn_food = 1;
         }
 
         // Move the snake's body
-        for (int i = 1; i < length; i++)
+        for (uint32_t i = 1; i < length; i++)
         {
             uint8_t tempYX[2];
-            tempYX[Y] = snakeLYX[i][0];
-            tempYX[X] = snakeLYX[i][1];
-            snakeLYX[i][0] = prev_segmentYX[Y];
-            snakeLYX[i][1] = prev_segmentYX[X];
+            tempYX[Y] = snakeLYX[i][Y];
+            tempYX[X] = snakeLYX[i][X];
+            snakeLYX[i][Y] = prev_segmentYX[Y];
+            snakeLYX[i][X] = prev_segmentYX[X];
             prev_segmentYX[Y] = tempYX[Y];
             prev_segmentYX[X] = tempYX[X];
         }
 
         // Place the snake on the matrix
-        for (int i = 0; i < length; i++)
+        for (uint32_t i = 0; i < length; i++)
         {
-            matrix[snakeLYX[i][0]][snakeLYX[i][1]] = 1;
+            matrix[snakeLYX[i][Y]][snakeLYX[i][X]] = 1;
         }
 
         // Place the food on the matrix
@@ -338,7 +391,7 @@ int Play_Snake(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint8_t button_mask_
  *
  * @return The score when the game is over, otherwise -1
  */
-int Play_Pong(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint8_t button_mask_press, uint32_t time_now)
+int32_t Play_Pong(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint8_t button_mask_press, uint32_t time_now)
 {
     static uint32_t prev_time = 0;
 
@@ -349,7 +402,7 @@ int Play_Pong(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint8_t button_mask_p
 
     uint8_t player_size = 3; // size of the player paddle (must be odd)
 
-    static int score = 0;
+    static uint32_t score = 0;
 
     static uint8_t first_time_running = 1;
     if (first_time_running)
@@ -375,30 +428,30 @@ int Play_Pong(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint8_t button_mask_p
         clear_screen(matrix);
 
         // Check for player collisions
-        for (int i = 0; i < player_size / 2 + 1; i++)
+        for (uint32_t i = 0; i < (uint32_t)(player_size / 2 + 1); i++)
         {
-            if (ballYX[X] == playerLeftYX[X] && ballYX[Y] == (playerLeftYX[Y] + i))
+            if (ballYX[X] == playerLeftYX[X] && ballYX[Y] == (uint8_t)(playerLeftYX[Y] + i))
             {
                 velocityYX[X] = -velocityYX[X];
                 velocityYX[Y] = i;
                 score++;
                 break;
             }
-            if (ballYX[X] == playerLeftYX[X] && ballYX[Y] == (playerLeftYX[Y] - i))
+            if (ballYX[X] == playerLeftYX[X] && ballYX[Y] == (uint8_t)(playerLeftYX[Y] - i))
             {
                 velocityYX[X] = -velocityYX[X];
                 velocityYX[Y] = -i;
                 score++;
                 break;
             }
-            if (ballYX[X] == playerRightYX[X] && ballYX[Y] == (playerRightYX[Y] + i))
+            if (ballYX[X] == playerRightYX[X] && ballYX[Y] == (uint8_t)(playerRightYX[Y] + i))
             {
                 velocityYX[X] = -velocityYX[X];
                 velocityYX[Y] = i;
                 score++;
                 break;
             }
-            if (ballYX[X] == playerRightYX[X] && ballYX[Y] == (playerRightYX[Y] - i))
+            if (ballYX[X] == playerRightYX[X] && ballYX[Y] == (uint8_t)(playerRightYX[Y] - i))
             {
                 velocityYX[X] = -velocityYX[X];
                 velocityYX[Y] = -i;
@@ -449,7 +502,7 @@ int Play_Pong(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint8_t button_mask_p
     // Place the ball on the matrix
     matrix[ballYX[Y]][ballYX[X]] = 1;
     // Place the players on the matrix
-    for (int i = 0; i < player_size / 2 + 1; i++)
+    for (uint32_t i = 0; i < (uint32_t)(player_size / 2 + 1); i++)
     {
         matrix[playerLeftYX[Y] - i][playerLeftYX[X]] = 1;
         matrix[playerLeftYX[Y] + i][playerLeftYX[X]] = 1;
@@ -472,7 +525,7 @@ int Play_Pong(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint8_t button_mask_p
  *
  * @return -4 if the game is still running, otherwise the score
  */
-int Play_FlappyBird(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint8_t button_mask_click, uint32_t time_now)
+int32_t Play_FlappyBird(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint8_t button_mask_click, uint32_t time_now)
 {
     // If this is the first time running, clear the screen
     static uint8_t first_time_running = 1;
@@ -483,10 +536,10 @@ int Play_FlappyBird(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint8_t button_
     }
 
     static uint32_t prev_time = 0;
-    static int count = 0;
-    static int score = -3;
+    static uint32_t count = 0;
+    static int32_t score = -3;
 
-    static int birdYX[2] = {7, 2};
+    static int32_t birdYX[2] = {7, 2};
 
     static uint8_t jump_requested = 0;
     if (button_mask_click)
@@ -502,14 +555,14 @@ int Play_FlappyBird(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint8_t button_
         matrix[birdYX[Y]][birdYX[X]] = 0;
 
         // Shift the entire screen to the left
-        for (int i = 0; i < SCREEN_HEIGHT; i++)
+        for (uint32_t i = 0; i < SCREEN_HEIGHT; i++)
         {
-            for (int j = 0; j < SCREEN_WIDTH - 1; j++)
+            for (uint32_t j = 0; j < SCREEN_WIDTH - 1; j++)
             {
                 matrix[i][j] = matrix[i][j + 1];
             }
         }
-        for (int i = 0; i < SCREEN_HEIGHT; i++)
+        for (uint32_t i = 0; i < SCREEN_HEIGHT; i++)
         {
             matrix[i][SCREEN_WIDTH - 1] = 0;
         }
@@ -517,12 +570,12 @@ int Play_FlappyBird(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint8_t button_
 
         if (count >= 5)
         {
-            int random_height = rand() % (SCREEN_HEIGHT - GAP_SIZE - 1) + 1;
-            for (int i = 0; i < random_height; i++)
+            uint32_t random_height = rand() % (SCREEN_HEIGHT - GAP_SIZE - 1) + 1;
+            for (uint32_t i = 0; i < random_height; i++)
             {
                 matrix[SCREEN_HEIGHT - i - 1][SCREEN_WIDTH - 1] = 1;
             }
-            for (int i = 0; i < SCREEN_HEIGHT - random_height - GAP_SIZE; i++)
+            for (uint32_t i = 0; i < SCREEN_HEIGHT - random_height - GAP_SIZE; i++)
             {
                 matrix[i][SCREEN_WIDTH - 1] = 1;
             }
@@ -531,7 +584,7 @@ int Play_FlappyBird(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint8_t button_
         }
 
         // Jump the bird if requested
-        static double velocity = 0;
+        static int32_t velocity = 0;
         if (jump_requested)
         {
             velocity = JUMP;
@@ -565,13 +618,17 @@ int Play_FlappyBird(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint8_t button_
     return -4;
 }
 
-int Play_Tetris(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint8_t button_mask_click)
+int32_t Play_Tetris(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint8_t button_mask_click)
 {
-    if (button_mask_click)
+    static uint32_t prev_time = 0;
+
+    // If this is the first time running, clear the screen
+    static uint8_t first_time_running = 1;
+    if (first_time_running)
     {
+        clear_screen(matrix);
+        first_time_running = 0;
     }
-    HeartMatrix(matrix);
-    return 0;
 }
 
 /**
@@ -585,9 +642,9 @@ int Play_Tetris(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint8_t button_mask
  */
 void MainMenuMatrix(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH])
 {
-    for (int i = 0; i < SCREEN_HEIGHT; i++)
+    for (uint32_t i = 0; i < SCREEN_HEIGHT; i++)
     {
-        for (int j = 0; j < SCREEN_WIDTH; j++)
+        for (uint32_t j = 0; j < SCREEN_WIDTH; j++)
         {
             matrix[i][j] = menu_matrix[i][j];
         }
@@ -607,29 +664,29 @@ void MainMenuMatrix(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH])
  */
 void ScoreMatrix(uint8_t matrix[SCREEN_HEIGHT][SCREEN_WIDTH], uint16_t score)
 {
-    for (int i = 0; i < SCREEN_HEIGHT; i++)
+    for (uint32_t i = 0; i < SCREEN_HEIGHT; i++)
     {
-        for (int j = 0; j < SCREEN_WIDTH; j++)
+        for (uint32_t j = 0; j < SCREEN_WIDTH; j++)
         {
             matrix[i][j] = score_matrix[i][j];
         }
     }
 
     uint8_t digits[4] = {(score / 1000) % 10, (score / 100) % 10, (score / 10) % 10, score % 10};
-    for (int dig = 0; dig < 4; dig++)
+    for (uint32_t dig = 0; dig < 4; dig++)
     {
-        for (int i = 9; i < 14; i++)
+        for (uint32_t i = 9; i < 14; i++)
         {
-            for (int j = dig * 4 + 1; j < dig * 4 + 4; j++)
+            for (uint32_t j = dig * 4 + 1; j < dig * 4 + 4; j++)
             {
                 matrix[i][j] = digit_matrices[digits[dig]][i - 9][j - (dig * 4 + 1)];
             }
         }
     }
 
-    for (int i = (SCREEN_HEIGHT / 2) + 7; i < SCREEN_HEIGHT; i++)
+    for (uint32_t i = (SCREEN_HEIGHT / 2) + 7; i < SCREEN_HEIGHT; i++)
     {
-        for (int j = 0; j < SCREEN_WIDTH; j++)
+        for (uint32_t j = 0; j < SCREEN_WIDTH; j++)
         {
             matrix[i][j] = score_matrix[i][j];
         }
